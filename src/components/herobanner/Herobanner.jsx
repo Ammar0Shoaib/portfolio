d45fff6355a2f419dc5c26353abe7279c43a41ba
ammarshoaib <ammarshoaib18@outlook.com>
2023-08-02 17:05:47 +0500

    here i add images and jsx in herobanner

--- src/components/herobanner/Herobanner.jsx
+++ src/components/herobanner/Herobanner.jsx
@@ -2,10 +2,65 @@ import React from 'react'
 import './Herobanner.css';
 
 
+import facebook from './banner-images/facebook.png';
+import google from './banner-images/google.png';
+import instagram from './banner-images/instagram.png';
+import twitter from './banner-images/twitter.png';
+import github from './banner-images/github.png';
+import imageUrl from './banner-images/printer.png';
+
+
 const Herobanner = () => {
-  return (
-    <div>Herobanner</div>
-  )
+    return (
+        <>
+            <div className="container">
+
+
+
+                <div className='social-icons'>
+
+                    <div className='social-images'>
+                        <div className="facebook">
+                            <img src={facebook} alt="facebook" />
+                        </div>
+                        <div className="google">
+                            <img src={google} alt="google" />
+                        </div>
+                        <div className="instagram">
+                            <img src={instagram} alt="instagram" />
+                        </div>
+                        <div className="twitter">
+                            <img src={twitter} alt="twitter" />
+                        </div>
+                        <div className="github">
+                            <img src={github} alt="twitter" />
+                        </div>
+                    </div>
+
+                </div>
+
+                <section className='introduction'>
+
+                    <p>Hello, I am </p>
+                    <h1>Ammar Shoaib</h1>
+                    <h3>Frontend Designer &nbsp;  &#10072; &nbsp; Developer</h3>
+
+                    <div className="btncontainer">
+                        <button className="button-with-image">
+                            <img src={imageUrl} alt="Icon" />
+                            <span className="button-text">Print Resume</span>
+                        </button>
+                    </div>
+
+
+                </section>
+
+
+            </div>
+
+
+        </>
+    )
 }
 
 export default Herobanner
