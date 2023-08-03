eb19c1403074af9ef100b20ceca322423628057a
ammarshoaib <ammarshoaib18@outlook.com>
2023-08-03 21:11:15 +0500

    start working on card c ...

--- src/components/card-c/Cardc.jsx
+++ src/components/card-c/Cardc.jsx
@@ -3,11 +3,28 @@ import React from 'react'
 import './Cardc.css';
 
 
-let wml = ' Who am I ?';
 
-let wdd = 'A Web Designer / Developer Located In Our Lovely Earth';
+import digimar from  './cardc-img/digimar.PNG';
 
-let para = 'Lorem ipsum dolor sit amet, consectetur otes yke manehij adipisicing elit.sit amet, Qui deserunt consequatur fugit repellendusillo voluptas?';
+import uxui from  './cardc-img/uxui.PNG';
+
+import webdpl from  './cardc-img/webdpl.PNG';
+
+
+
+
+
+
+
+
+
+const wml = 'My Expertise';
+
+const wdd = 'A Web Designer / Developer Located In Our Lovely Earth';
+
+const para = 'Lorem ipsum dolor sit amet, consectetur otes yke manehij adipisicing elit.sit amet, Qui deserunt consequatur fugit repellendusillo voluptas?';
+
+
 
 
 
@@ -17,9 +34,9 @@ const Cardc = () => {
             <div className="Cardc-container">
                 <h3 className='wml'>{wml}</h3>
                 <span className="Cardc-container_line"></span>
-                <h5 className="intro">{wdd}</h5>
-                <p className="brief">{para}</p>
-                <button className="btn"><i></i>Download My CV</button>
+
+
+
             </div>
         </>
     )
