6a61e9bc4f3d71a0b1ca9d7f6ab42541e1b014b4
ammarshoaib <ammarshoaib18@outlook.com>
2023-08-04 12:41:31 +0500

    every thing is clear in card c

--- src/components/card-c/Cardc.jsx
+++ src/components/card-c/Cardc.jsx
@@ -4,13 +4,11 @@ import './Cardc.css';
 
 
 
-import digimar from  './cardc-img/digimar.PNG';
+// import digimar from './cardc-img/digimar.PNG';
 
-import uxui from  './cardc-img/uxui.PNG';
+// import uxui from './cardc-img/uxui.PNG';
 
-import webdpl from  './cardc-img/webdpl.PNG';
-
-
+// import webdpl from './cardc-img/webdpl.PNG';
 
 
 
@@ -18,11 +16,13 @@ import webdpl from  './cardc-img/webdpl.PNG';
 
 
 
-const wml = 'My Expertise';
 
-const wdd = 'A Web Designer / Developer Located In Our Lovely Earth';
 
-const para = 'Lorem ipsum dolor sit amet, consectetur otes yke manehij adipisicing elit.sit amet, Qui deserunt consequatur fugit repellendusillo voluptas?';
+const mexp = 'My';
+
+// const wdd = 'A Web Designer / Developer Located In Our Lovely Earth';
+
+// const para = 'Lorem ipsum dolor sit amet, consectetur otes yke manehij adipisicing elit.sit amet, Qui deserunt consequatur fugit repellendusillo voluptas?';
 
 
 
@@ -32,8 +32,54 @@ const Cardc = () => {
     return (
         <>
             <div className="Cardc-container">
-                <h3 className='wml'>{wml}</h3>
-                <span className="Cardc-container_line"></span>
+                <h3 className='wml'>{mexp}</h3>
+                {/* <span className="Cardc-container_line"></span> */}
+
+
+                {/* <div className="cardc_sub-container">
+
+
+                    <div className="imgplustext1">
+                        <div className="img1">
+                            <img src={digimar} alt="digital-marketing" />
+                            <div className="text1">
+                                <div className="headingpluspara">
+                                    <h6>UX Design</h6>
+                                    <p>exercitat Repellendus, corrupt.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="imgplustext2">
+                        <div className="img2">
+                            <img src={uxui} alt="UX-UI-design" />
+                            <div className="text2">
+                                <div className="headingpluspara">
+                                    <h6>UX Design</h6>
+                                    <p>exercitat Repellendus, corrupt.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="imgplustext2">
+                        <div className="img2">
+                            <img src={webdpl} alt="web-development" />
+                            <div className="text3">
+                                <div className="headingpluspara">
+                                    <h6>UX Design</h6>
+                                    <p>exercitat Repellendus, corrupt.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div> */}
+
 
 
 
